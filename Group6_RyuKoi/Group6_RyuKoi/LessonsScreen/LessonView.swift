2831f5b3cf2e5721f588e63686f3ebfee3942eff
roslyn-maloney <maloney.r@northeastern.edu>
2025-12-10 10:28:19 -0500

    Got rid of favoriting on lesson scree

--- Group6_RyuKoi/Group6_RyuKoi/LessonsScreen/LessonView.swift
+++ Group6_RyuKoi/Group6_RyuKoi/LessonsScreen/LessonView.swift
@@ -6,7 +6,6 @@ class LessonView: UIView {
     var lessonName: UILabel!
     var descriptionContainer: UIView!
     var descriptionLabel: UILabel!
-    var startBtn: UIButton!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -16,7 +15,6 @@ class LessonView: UIView {
         setupLessonName()
         setupDescriptionContainer()
         setupDescriptionLabel()
-        setupStartBtn()
         initConstraints()
     }
     
@@ -60,16 +58,6 @@ class LessonView: UIView {
         descriptionContainer.addSubview(descriptionLabel)
     }
     
-    func setupStartBtn() {
-        startBtn = UIButton()
-        startBtn.setTitle("Start", for: .normal)
-        startBtn.setTitleColor(.white, for: .normal)
-        startBtn.backgroundColor = UIColor(red: 59/255, green: 9/255, blue: 24/255, alpha: 1.0)
-        startBtn.layer.cornerRadius = 5
-        startBtn.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(startBtn)
-    }
-    
 
     func initConstraints() {
         NSLayoutConstraint.activate([
@@ -92,11 +80,6 @@ class LessonView: UIView {
             
             descriptionLabel.topAnchor.constraint(equalTo: descriptionContainer.topAnchor, constant: 16),
             descriptionLabel.centerXAnchor.constraint(equalTo: descriptionContainer.centerXAnchor),
-            
-            startBtn.topAnchor.constraint(equalTo: descriptionContainer.bottomAnchor, constant: 20),
-            startBtn.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor),
-            startBtn.widthAnchor.constraint(equalToConstant: 150),
-            startBtn.heightAnchor.constraint(equalToConstant: 50),
             
         ])
     }
